686f3c7cd866b39e1588753c6d6fa68a8724c0ce
Ashutosh Korde <ashutoshkorde777@gmail.com>
2025-01-17 11:15:18 +0530

    updated uplode directory

--- backend/config.js
+++ backend/config.js
@@ -7,7 +7,7 @@ import mysql from "mysql2";
 const pool = mysql.createPool({
     host: '127.0.0.1',
     user: 'root',
-    password: "Chinmay@1824",
+    password: "Ashutosh@2003",
     database: 'commonDB1'
 }).promise()
 
--- backend/db/index.js
+++ backend/db/index.js
@@ -4,7 +4,7 @@ import ApiError from '../utils/ApiError.js'
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: '',
+  password: 'Ashutosh@2003',
   database: 'commonDB1',
 })
 
