a5669ffd4675eb24c5576ce6f4034c194264c118
Krishna Magar <magarkrishna2564@gmail.com>
2025-01-21 15:42:02 +0530

    commit

--- backend/config.js
+++ backend/config.js
@@ -1,14 +1,16 @@
 // config.js
-import dotenv from 'dotenv';
-dotenv.config();
+import dotenv from 'dotenv'
+dotenv.config()
 
-import mysql from "mysql2";
+import mysql from 'mysql2'
 
-const pool = mysql.createPool({
+const pool = mysql
+  .createPool({
     host: '127.0.0.1',
     user: 'root',
-    password: "Ashutosh@2003",
-    database: 'commonDB1'
-}).promise()
+    password: '',
+    database: 'aakarerp',
+  })
+  .promise()
 
-export default pool;
+export default pool
