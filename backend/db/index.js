1be9fde744533d17b96ed779ff4f93a777e225a8
Ashutosh Korde <ashutoshkorde777@gmail.com>
2025-01-21 15:44:17 +0530

    update assignee route

--- backend/db/index.js
+++ backend/db/index.js
@@ -4,13 +4,8 @@ import ApiError from '../utils/ApiError.js'
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-<<<<<<< HEAD
   password: 'Ashutosh@2003',
   database: 'commondb1',
-=======
-  password: 'Harita2*',
-  database: 'commonDB1',
->>>>>>> 91d4c4e7b407ac22f13a9b5c147a115804d25ee8
 })
 
 connection.connect((err) => {
